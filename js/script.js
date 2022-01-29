aa47e271ac6262b17f8dae69fd59a4e0eb26f2ee
tera-ono <95453431+tera-ono@users.noreply.github.com>
2022-01-29 15:01:15 +0900

    finish site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/* --- aos.js スクロールアニメーション --- */
+AOS.init({
+  /* --- 一度のみの発火 --- */
+  offset: 100,
+  once: true,
+});
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
@@ -10,7 +16,15 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $('body').toggleClass('is-open');
   });
 
+  $('.js-drawer-link').on('click', function() {
+    $('.js-drawer').removeClass('is-open');
+    $('.js-mobile-menu').removeClass('is-open');
+    $('.js-header').removeClass('is-open');
+    /* --- 背景スクロール禁止&解除 --- */
+    $('body').removeClass('is-open');
+  });
 
+  /* --- トップへ戻るボタン --- */
   var topBtn = $('.pagetop');
   topBtn.hide();
 
@@ -33,10 +47,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     return false;
   });
 
-
-
-  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
-
+  /* --- スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動) --- */
   $(document).on('click', 'a[href*="#"]', function () {
     let time = 400;
     let header = $('header').innerHeight();
@@ -50,27 +61,27 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
   /* スクロールしてMVが見えなくなった所でヘッダーが固定して追従する
   ===================================================*/
-  // let header = $('.js-header');
-  // let header_height = header.innerHeight();
-  // let mv_height = $('.js-mv').innerHeight();
-  // let target = header_height + mv_height;
-  // var headNav = $("header");
-	// //scrollだけだと読み込み時困るのでloadも追加
-	// $(window).on('load scroll', function () {
-	// 	//現在の位置が500px以上かつ、クラスfixedが付与されていない時
-	// 	if($(this).scrollTop() > target && header.hasClass('fixed') == false) {
-	// 		//headerの高さ分上(マイナス)に設定
-	// 		header.css({"top": -(header_height)});
-	// 		//クラスfixedを付与
-	// 		header.addClass('fixed');
-	// 		//位置を0に設定し、アニメーションのスピードを指定
-	// 		header.animate({"top": 0},600);
-	// 	}
-	// 	//現在の位置が300px以下かつ、クラスfixedが付与されている時にfixedを外す
-	// 	else if($(this).scrollTop() < target && header.hasClass('fixed') == true){
-	// 		header.removeClass('fixed');
-	// 	}
-  // });
-
+  let header = $('.js-header');
+  let header_height = header.innerHeight();
+  let mv_height = $('.js-mv').innerHeight();
+  let target = header_height + mv_height;
+  var headNav = $("header");
+	//scrollだけだと読み込み時困るのでloadも追加
+	$(window).on('load scroll', function () {
+		//現在の位置がtarget以上かつ、クラスfixedが付与されていない時
+		if($(this).scrollTop() > target && header.hasClass('fixed') == false) {
+			//headerの高さ分上(マイナス)に設定
+			header.css({"top": -(header_height)});
+			//クラスfixedを付与
+			header.addClass('fixed');
+			//位置を0に設定し、アニメーションのスピードを指定
+			header.animate({"top": 0},600);
+		}
+		//現在の位置がtarget以下かつ、クラスfixedが付与されている時にfixedを外す
+		else if($(this).scrollTop() < target && header.hasClass('fixed') == true){
+			header.removeClass('fixed');
+		}
+  });
 
 });
+
